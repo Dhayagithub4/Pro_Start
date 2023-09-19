f59a6bd6d13dd39f0a9ab3a08c1c1ea3f14ebc06
Dhayagithub4 <pdhayalan444@gmail.com>
2023-09-19 21:05:28 +0530

    Micahel code

--- src/test/java/org/jonas/Test.java
+++ src/test/java/org/jonas/Test.java
@@ -2,8 +2,8 @@ package org.jonas;
 
 public class Test {
 private void tests() {
-	System.out.println("Test1");
-	System.out.println("Test2");
+	System.out.println("Test4");
+	System.out.println("Test5");
 }
 		
 	}
