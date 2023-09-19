36b6d5c6e0ef6a6a5f1aecd74244c5f8f936fe9d
Dhayagithub4 <pdhayalan444@gmail.com>
2023-09-19 21:41:17 +0530

    jonass code

--- src/test/java/org/jonas/Test.java
+++ src/test/java/org/jonas/Test.java
@@ -2,9 +2,10 @@ package org.jonas;
 
 public class Test {
 private void tests() {
-	System.out.println("Test3");
 	System.out.println("Test4");
+	System.out.println("Test5");
 	System.out.println("Test1");
+	System.out.println("Test2");
 }
 		
 	}
